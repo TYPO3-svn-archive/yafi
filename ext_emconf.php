031e259734b0733e1c31a2bff7ed83394cde1b18
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-05-02 14:50:08 +0000

    0.4.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/yafi/trunk@9070 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "yafi"
 #
-# Auto generated 23-04-2008 13:08
+# Auto generated 02-05-2008 17:49
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'Netcreators BV',
-	'version' => '0.2.0',
+	'version' => '0.4.0',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:24:{s:9:"ChangeLog";s:4:"4492";s:21:"class.tx_yafi_api.php";s:4:"5c56";s:27:"class.tx_yafi_feed_info.php";s:4:"6de5";s:27:"class.tx_yafi_feed_item.php";s:4:"35ce";s:12:"ext_icon.gif";s:4:"0880";s:17:"ext_localconf.php";s:4:"fec4";s:14:"ext_tables.php";s:4:"cc43";s:14:"ext_tables.sql";s:4:"bfec";s:21:"icon_tx_yafi_feed.gif";s:4:"0880";s:25:"icon_tx_yafi_importer.gif";s:4:"2224";s:30:"interface.tx_yafi_importer.php";s:4:"d547";s:17:"locallang_csh.xml";s:4:"8040";s:16:"locallang_db.xml";s:4:"1a0b";s:7:"tca.php";s:4:"df34";s:16:"cli/cli_yafi.php";s:4:"30e1";s:43:"importers/class.tx_yafi_ttnews_importer.php";s:4:"8624";s:46:"importers/flexform_tx_yafi_ttnews_importer.xml";s:4:"f8bf";s:44:"importers/flexform_tx_yafi_zero_importer.xml";s:4:"5ee8";s:25:"lib/simplepie/LICENSE.txt";s:4:"2377";s:27:"lib/simplepie/simplepie.inc";s:4:"dae5";s:25:"pi1/class.tx_yafi_pi1.php";s:4:"94b8";s:17:"pi1/locallang.xml";s:4:"65fe";s:46:"static/yet_another_feed_importer/constants.txt";s:4:"f0cb";s:42:"static/yet_another_feed_importer/setup.txt";s:4:"c282";}',
+	'_md5_values_when_last_written' => 'a:26:{s:9:"ChangeLog";s:4:"4492";s:21:"class.tx_yafi_api.php";s:4:"30e8";s:27:"class.tx_yafi_feed_info.php";s:4:"6de5";s:27:"class.tx_yafi_feed_item.php";s:4:"35ce";s:12:"ext_icon.gif";s:4:"0880";s:17:"ext_localconf.php";s:4:"fec4";s:14:"ext_tables.php";s:4:"a81c";s:14:"ext_tables.sql";s:4:"9f24";s:21:"icon_tx_yafi_feed.gif";s:4:"0880";s:25:"icon_tx_yafi_importer.gif";s:4:"2224";s:30:"interface.tx_yafi_importer.php";s:4:"8d4b";s:17:"locallang_csh.xml";s:4:"8040";s:16:"locallang_db.xml";s:4:"e97f";s:7:"tca.php";s:4:"0753";s:16:"cli/cli_yafi.php";s:4:"2cfd";s:43:"importers/class.tx_yafi_ttnews_importer.php";s:4:"da9c";s:46:"importers/flexform_tx_yafi_ttnews_importer.xml";s:4:"f9ac";s:44:"importers/flexform_tx_yafi_zero_importer.xml";s:4:"5ee8";s:25:"lib/simplepie/LICENSE.txt";s:4:"2377";s:35:"lib/simplepie/add-typo3-proxy.patch";s:4:"4897";s:27:"lib/simplepie/simplepie.inc";s:4:"7495";s:36:"lib/simplepie/simplepie.inc.original";s:4:"2a04";s:25:"pi1/class.tx_yafi_pi1.php";s:4:"94b8";s:17:"pi1/locallang.xml";s:4:"65fe";s:46:"static/yet_another_feed_importer/constants.txt";s:4:"145e";s:42:"static/yet_another_feed_importer/setup.txt";s:4:"ad8b";}',
 	'suggests' => array(
 	),
 );
